337a37fb51fad11d434e11fef1d8d6d3ca38af6b
NoppharatDev <noppharat.dev@gmail.com>
2022-09-02 11:43:01 +0700

    Update check quest in event

--- src/quests/quests.module.ts
+++ src/quests/quests.module.ts
@@ -3,9 +3,10 @@ import { QuestsService } from './quests.service';
 import { QuestsController } from './quests.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Quest } from './entities/quest.entity';
+import { CheckPoint } from 'src/checkpoints/entities/checkpoint.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Quest])],
+  imports:[TypeOrmModule.forFeature([Quest, CheckPoint])],
   controllers: [QuestsController],
   providers: [QuestsService]
 })
